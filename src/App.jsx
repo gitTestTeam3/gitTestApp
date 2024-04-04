7428bfcb5ed87614780452a6a16d4a8f4b133054
alsendrha <alsendrha@naver.com>
2024-04-04 22:23:35 +0900

    ddasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const clientId =
+    "459676653842-gimcu9k68ij6s3m1d7u059bv4b7qskq1.apps.googleusercontent.com";
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <GoogleOAuthProvider clientId={clientId}>
+          <GoogleLogin
+            onSuccess={(res) => {
+              console.log(res);
+            }}
+            onFailure={(err) => {
+              console.log(err);
+            }}
+          />
+        </GoogleOAuthProvider>
+        <button>구글로그인</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
