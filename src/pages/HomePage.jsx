a751ead52cf2b16677296fb6f1d1f35cb585d889
taeyuls <startez03@gmail.com>
2024-04-04 22:39:22 +0900

    button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <button>눌러줘</button>
+    </div>
+  );
 };
 
 export default HomePage;
