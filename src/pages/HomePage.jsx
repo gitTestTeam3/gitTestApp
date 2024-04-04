410154d55ff4cdfffa00d8a21ffc34bb48627a65
taeyuls <startez03@gmail.com>
2024-04-04 22:50:34 +0900

    test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
-
-  return (
-    <div>
-      HomePage
-      <button>눌러줘</button>
-    </div>
-  );
-
+  return <div>netmarble</div>;
 };
 
 export default HomePage;
