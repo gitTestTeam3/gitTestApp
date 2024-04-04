40b1f246dbca520facfdd1727fe607a9c330dba1
taeyuls <startez03@gmail.com>
2024-04-04 23:34:33 +0900

    test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>netmarble</div>;
+  return (
+    <div>
+      netmarble<h1>료니는 귀여워</h1>
+      <button>추천</button>
+    </div>
+  );
 };
 
 export default HomePage;
--- src/pages/ryuni.jsx
+++ src/pages/ryuni.jsx
@@ -4,6 +4,15 @@ const ryuni = () => {
   return (
     <div>
       <h1>료니는 귀여워</h1>
+      <button>추천</button>
+      <button
+        onClick={() => {
+          ryuni();
+        }}
+      >
+        반대
+      </button>
+      const [ryoni, setRyoni] = useState("")
     </div>
   );
 };
